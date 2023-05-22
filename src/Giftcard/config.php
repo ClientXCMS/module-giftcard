4ee176ab164dc5a8dd8093355322751fd49dc9e6
Martindev <Delebecque.martin@gmail.com>
2023-05-22 23:27:28 +0200

    Update config.php

--- src/Giftcard/config.php
+++ src/Giftcard/config.php
@@ -5,5 +5,7 @@ use function DI\get;
 
 return [
     'admin.menu.items' => add(get(\App\Giftcard\GiftcardAdminItem::class)),
+    
+    'permissions.list' => add([\App\Giftcard\Actions\GiftcardCrudAction::class => 'Giftcard']),
     \App\Giftcard\Actions\GiftAddCard::class => \DI\autowire()->constructorParameter('currency', get('app.currency')),
 ];
